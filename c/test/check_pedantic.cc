c81f7f1e3ce0e8a01c198fe7d30257de0836d0ff
Sam Lewis <sam.vr.lewis@gmail.com>
2021-06-09 18:05:21 +1000

    Fix lint error

--- c/test/check_pedantic.cc
+++ c/test/check_pedantic.cc
@@ -1,7 +1,7 @@
+#include <ixcom/XCOMdat.h>
 #include <ixcom/decode.h>
 #include <ixcom/encode.h>
 #include <ixcom/messages.h>
-#include <ixcom/XCOMdat.h>
 
 /*
  * This file exists to ensure that all library includes are valid C++.
