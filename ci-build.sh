77dffaa5eefa064ce3a229b5f6739473ef79da94
Isaac Torres <isaac@isaactorz.dev>
2022-09-27 14:52:47 -0600

    Don't download scanner again

--- ci-build.sh
+++ ci-build.sh
@@ -39,12 +39,6 @@ function build_codecov() {
     fi
 
     export SONAR_SCANNER_HOME="${HOME}/.sonar/sonar-scanner-${SONAR_SCANNER_VERSION}-linux"
-
-    # download sonar-scanner
-    curl -sSLo "${HOME}/.sonar/sonar-scanner.zip" \
-      "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-linux.zip"
-    unzip -o "${HOME}/.sonar/sonar-scanner.zip" -d "${HOME}/.sonar/"
-    export PATH=${SONAR_SCANNER_HOME}/bin:${PATH}
     export SONAR_SCANNER_OPTS="-server"
 
     # Run sonar scanner
